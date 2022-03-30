f31dfdcbbafcf5b5645dbdbe22ea04c2a9f0887f
Fefaodessa <fefaodessa@gmail.com>
2022-03-30 23:20:47 +0300

    DZ 9 TEST 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
 
+  const refs = {
+      body : document.querySelector('body'),
+      btnStart : document.querySelector('[data-start]'),
+      btnStop : document.querySelector('[data-stop]'),
+  }
+
+refs.btnStart.addEventListener("click", onStart)
+
+function  onStart () {
+    timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    refs.btnStart.disabled = true;
+}
+
+refs.btnStop.addEventListener("click", ofStart);
+
+function ofStart (){
+    clearInterval(timerId);
+    refs.btnStart.disabled = false;
+}
