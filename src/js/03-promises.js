d668acb240486d85c6299e30fcdf6b8a7774dfdf
Fefaodessa <fefaodessa@gmail.com>
2022-04-05 17:10:02 +0300

    DZ 9 Test 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return  new Promise((resolve, reject) => {
+    setTimeout(() => {
+    if (shouldResolve) {
+      resolve(Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`));
+    } else {
+      reject(Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+  })
 }
+
+  const form = document.querySelector(".form");
+  const delayEl = form.delay;
+  const stepEl = form.step;
+  const amountEl = form.amount;
+
+  form.addEventListener("submit", stop)
+
+  function stop (event){
+    event.preventDefault();
+    for (let i = 0; i < amountEl.value; i += 1) {
+      const time = Number(delayEl.value) + Number((i * stepEl.value));
+      const position = i + 1;
+      createPromise(position, time)
+      .then(resolve => {
+        resolve;
+      })
+      .catch(reject => {
+        reject;
+      });
+    } 
+  }
